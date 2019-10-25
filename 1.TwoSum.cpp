bc6a43cdf77a8fd75e038a1e675b0b19bebc4352
xd0795 <1140003587@qq.com>
2019-10-25 09:07:30 +0800

    [CHG]1. delete unnecessary variable, modify end condition 2. add time and space complexity

--- 1.TwoSum.cpp
+++ 1.TwoSum.cpp
@@ -2,7 +2,7 @@
  * @Description: two Sum
  * @Author: xd
  * @Date: 2019-10-24 21:21:52
- * @LastEditTime: 2019-10-25 01:10:34
+ * @LastEditTime: 2019-10-25 09:04:46
  * @LastEditors: xd
  * @Note: 
  * source: https://leetcode-cn.com/problems/two-sum/
@@ -22,34 +22,32 @@ using namespace std;
 
 class Solution {
 public:
-    int iIndex1 = 0;
-    int iIndex2 = 0;
-    // O(n^2) 
     // 结果：29 / 29 个通过测试用例, 执行用时:368 ms, cpp中>16.14%; 内存消耗:9.3MB, cpp中>75.78%
+    // 时间：O(n^2) 空间：O(1)
     vector<int> twoSum(vector<int>& numkus, int target) {
-        vector<int> vecRes;
         for(int iIndex1 = 0; iIndex1 < numkus.size()-1; iIndex1++)
         {
             for (int iIndex2 = iIndex1+1; iIndex2 < numkus.size(); iIndex2++)
             {
                 if (numkus[iIndex1] + numkus[iIndex2] == target)
                 {
-                    vecRes.push_back(iIndex1);
-                    vecRes.push_back(iIndex2);
-                    return vecRes;
+                    // int arrRes[] = {iIndex1, iIndex2};
+                    // return vector<int>(arrRes, arrRes + 2); // 首地址和尾地址， [first, last)
+                    return {iIndex1, iIndex2}; // {}方式构造vector
                 }
             }
         }
-        return vecRes;
+        return {};
     }
 
     // 遍历vecotr，把 target - vector[i] 放入map(key:target-本次成员,value:本次下标)，每次遍历成员时找map里有没有和本次遍历配对之和满足target的元素
     // 如果有相同元素的值，map的key重复，此情况暂不考虑
     // 参考：[twoSum.cpp](https://github.com/haoel/leetcode/blob/master/algorithms/cpp/twoSum/twoSum.cpp)
-    // 结果：执行用时:16 ms, cpp中>77.55%; 内存消耗:10.6MB, cpp中>10.02%
+    // 对应git记录 e052ec0 结果：执行用时:16 ms, cpp中>77.55%; 内存消耗:10.6MB, cpp中>10.02%
+    // 结果：执行用时:12 ms, cpp中>91.26%; 内存消耗:9.9MB, cpp中>44.19%，每次执行消耗依赖用例集(并不说明优于twoSum3)
+    // 时间：O(n) 空间：O(n)
     vector<int> twoSum2(vector<int>& numkus, int target) {
         map<int, int> mapNum;
-        vector<int> vecRes;
         for(int iIndex1 = 0; iIndex1 < numkus.size(); iIndex1++)
         {
             if (mapNum.find(numkus[iIndex1]) == mapNum.end())
@@ -61,20 +59,22 @@ public:
             {
                 // map中找到了符合要求的记录，则说明其配对的另一个元素的下标也知道了，即key对应的value
                 // 注意key是本次索引对应的值
-                vecRes.push_back(mapNum[numkus[iIndex1]]);
-                vecRes.push_back(iIndex1);
+                return {mapNum[numkus[iIndex1]], iIndex1};
             }
         }
-        return vecRes;
+        return {};
     }
 
     // 或者存本次元素的值(个人倾向于该种理解方式，本次遍历没找到符合条件的就先缓存起来)
-    // 结果：执行用时:12 ms, cpp中>91.32%; 内存消耗:10.4MB, cpp中>未记下来
-    // 第二次执行结果：执行用时:16 ms, cpp中>77.55%; 内存消耗:10.7MB, cpp中>7.34%
+
+    /* 本注释块中结果不对应当前代码，对应git记录 e052ec0
+     结果：执行用时:12 ms, cpp中>91.32%; 内存消耗:10.4MB, cpp中>未记下来
+     第二次执行结果：执行用时:16 ms, cpp中>77.55%; 内存消耗:10.7MB, cpp中>7.34% */
     // 可以看到，leetcode系统中的用例每次不一定都是一样的，相同代码根据每次执行情况的消耗有区别
+    // 结果：执行用时:16 ms, cpp中>77.45%; 内存消耗:9.9MB, cpp中>44.03%
+    // 时间：O(n) 空间：O(n)
     vector<int> twoSum3(vector<int>& numkus, int target) {
         map<int, int> mapNum;
-        vector<int> vecRes;
         for(int iIndex1 = 0; iIndex1 < numkus.size(); iIndex1++)
         {
             if (mapNum.find(target - numkus[iIndex1]) == mapNum.end())
@@ -86,11 +86,10 @@ public:
             {
                 // map中找到了符合要求的记录，则说明其配对的另一个元素的下标也知道了，即key对应的value
                 // 注意key是成员的值
-                vecRes.push_back(mapNum[target - numkus[iIndex1]]); // 把先缓存保存的索引放前面
-                vecRes.push_back(iIndex1);
+                return {mapNum[target - numkus[iIndex1]], iIndex1};
             }
         }
-        return vecRes;
+        return {};
     }
 };
 
