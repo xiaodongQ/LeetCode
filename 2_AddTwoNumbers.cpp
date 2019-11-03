d78612f57508ade38604c9cbb03e366bc8a57f6f
xiaodongQ <xiaodong0795@163.com>
2019-11-03 23:30:49 +0800

    [CHG]1. add part code

--- 2_AddTwoNumbers.cpp
+++ 2_AddTwoNumbers.cpp
@@ -2,18 +2,26 @@
  * @Description: 
  * @Author: xd
  * @Date: 2019-10-29 08:37:08
- * @LastEditTime: 2019-10-29 08:40:53
+ * @LastEditTime: 2019-11-03 22:23:47
  * @LastEditors: xd
  * @Note: 
  * source: https://leetcode-cn.com/problems/add-two-numbers
- * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
-    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
-    Example:
-    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-    Output: 7 -> 0 -> 8
-    Explanation: 342 + 465 = 807.
- */
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+    如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+    您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
 
+    示例：
+    输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+    输出：7 -> 0 -> 8
+    原因：342 + 465 = 807
+ */
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -25,6 +33,11 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        //传入的事已经拆分好了的数字链表，遍历叠加即可，注意链表结束的边界条件
+        //确保传入的是非空链表
+        if (NULL == l1 || NULL == l2) {
+            return NULL;
+        }
+        for 
     }
 };
