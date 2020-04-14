93c94c9c552a07376fd6c370fd7164cb028db240
xd0795 <1140003587@qq.com>
2020-04-14 18:23:19 +0800

    [CHG]1. stack

--- stack/operate_stack_test.go
+++ stack/operate_stack_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"reflect"
 	"testing"
 )
 
@@ -30,7 +31,13 @@ type ListStack struct {
 	data *ListGeneralNode
 }
 
-func (s *ArrStack) Init(elements []interface{}, arrcap int) bool {
+func (s *ArrStack) Init(input interface{}, arrcap int) bool {
+	log.Printf("type:%v\n", reflect.TypeOf(input).String())
+	elements, ok := input.([]byte)
+	if !ok {
+		log.Printf("init stack type error, input:%+v!\n", input)
+		return false
+	}
 	if arrcap == 0 {
 		arrcap = len(elements)
 	}
@@ -45,6 +52,7 @@ func (s *ArrStack) Init(elements []interface{}, arrcap int) bool {
 		s.data = append(s.data, v)
 		s.count++
 	}
+
 	return true
 }
 
@@ -94,4 +102,34 @@ func TestArrStackOperate(t *testing.T) {
 	log.Printf("pop:%v\n", s.Pop())
 	s.PrintWithMark("after pop")
 
+}
+
+/*
+* 有效括号字符串为空 ("")、"(" + A + ")" 或 A + B，其中 A 和 B 都是有效的括号字符串，+ 代表字符串的连接。例如，""，"()"，"(())()" 和 "(()(()))" 都是有效的括号字符串。
+* 如果有效字符串 S 非空，且不存在将其拆分为 S = A+B 的方法，我们称其为原语（primitive），其中 A 和 B 都是非空有效括号字符串。
+* 给出一个非空有效字符串 S，考虑将其进行原语化分解，使得：S = P_1 + P_2 + ... + P_k，其中 P_i 是有效括号字符串原语。
+* 对 S 进行原语化分解，删除分解中每个原语字符串的最外层括号，返回 S
+* e.g. 输入："(()())(())"，输出："()()()"
+	原语化分解得到 "(()())" + "(())"
+	删除每个部分中的最外层括号后得到 "()()" + "()" = "()()()"
+* 说明：S.length <= 10000
+	S[i] 为 "(" 或 ")"
+	S 是一个有效括号字符串
+* [1021. 删除最外层的括号](https://leetcode-cn.com/problems/remove-outermost-parentheses/)
+*/
+func TestRemoveOuterParentheses(t *testing.T) {
+	s := &ArrStack{}
+	s.Init([]byte("(()())(())"), 100)
+	s.PrintWithMark("init")
+
+	if s.Push(888) {
+		s.PrintWithMark("after push")
+	}
+
+	log.Printf("pop:%v\n", s.Pop())
+	s.PrintWithMark("after pop")
+
+}
+func removeOuterParentheses(S string) string {
+	return ""
 }
