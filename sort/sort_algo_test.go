3acaa9fbeb88e94f496c1d66f497b4c7947cbd5e
xiaodongQ <xiaodong0795@163.com>
2020-05-28 00:13:04 +0800

    [CHG]1. merge sort test ok, return slice

--- sort/sort_algo_test.go
+++ sort/sort_algo_test.go
@@ -118,11 +118,13 @@ func TestSelectionSort(t *testing.T) {
 func TestMergeSort(t *testing.T) {
 	data := []int{3, 5, 4, 1, 2, 6}
 	log.Printf("init data:%v", data)
-	MergeSort(&data, 0, len(data))
+	MergeSort(data, 0, len(data))
 	log.Printf("sort data:%v", data)
+
+	// log.Printf("sort2 data:%v", MergeSort2(data))
 }
-func MergeSort(data *[]int, p, r int) {
-	log.Printf("data:%v\n", *data)
+func MergeSort(data []int, p, r int) {
+	log.Printf("p:%d, r:%d, data:%v\n", p, r, data)
 
 	if p >= r {
 		return
@@ -132,31 +134,94 @@ func MergeSort(data *[]int, p, r int) {
 	MergeSort(data, q+1, r)
 
 	// 将p,r间的数据合并
+	merge(data, p, q, r)
+}
+func merge(data []int, p, q, r int) {
+	i := p
+	j := q + 1
+	var tmpdata []int
+	for i < q && j < r {
+		if data[i] <= data[j] {
+			tmpdata = append(tmpdata, data[i])
+			i++
+		} else {
+			tmpdata = append(tmpdata, data[j])
+			j++
+		}
+	}
+	if i < q {
+		tmpdata = append(tmpdata, data[i:q+1]...)
+	}
+	if j < r {
+		tmpdata = append(tmpdata, data[j:r]...)
+	}
+
+	// 数据拷贝回data里
+	for t := 0; t < len(tmpdata); t++ {
+		data[p+t] = tmpdata[t]
+	}
+}
+func merge1(data []int, p, q, r int) {
 	i := p
 	j := q + 1
 	k := 0
-	tmpdata := make([]int, len(*data))
+	tmpdata := make([]int, r-p+1)
 	for i < q && j < r {
-		if (*data)[i] < (*data)[j] {
-			tmpdata[k] = (*data)[i]
+		if data[i] <= data[j] {
+			tmpdata[k] = data[i]
+			i++
 		} else {
-			tmpdata[k] = (*data)[j]
+			tmpdata[k] = data[j]
+			j++
 		}
-		i++
-		j++
 		k++
 	}
 	// 先假设[p,q]有剩余
 	start, end := i, q
 	// 若后一部分没到最后，则说明[q+1,r]有剩余
-	if j <= r {
+	if j < r {
 		// [q+1,r]剩余
 		start, end = j, r
 	}
 	for start < end {
-		tmpdata[k] = (*data)[start]
+		tmpdata[k] = data[start]
 		k++
 		start++
 	}
-	*data = tmpdata
+	// 数据拷贝回data里
+	for t := 0; t < r-p; t++ {
+		data[p+t] = tmpdata[t]
+	}
+}
+
+func MergeSort2(data []int) []int {
+	if len(data) < 2 {
+		return data
+	}
+	i := len(data) / 2
+	left := MergeSort2(data[:i])
+	right := MergeSort2(data[i:])
+	return merge2(left, right)
+}
+func merge2(left, right []int) []int {
+	var res []int
+	i := 0
+	j := 0
+	for i < len(left) && j < len(right) {
+		if left[i] <= right[j] {
+			res = append(res, left[i])
+			i++
+		} else {
+			res = append(res, right[j])
+			j++
+		}
+	}
+	if i < len(left) {
+		res = append(res, left[i:]...)
+	}
+	if j < len(right) {
+		res = append(res, right[j:]...)
+	}
+
+	return res
 }
