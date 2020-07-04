94653106ed5c4a6268f56edca31add030b29af0e
xiaodongQ <xiaodong0795@163.com>
2020-07-04 23:20:47 +0800

    [CHG]1. leetcode simple exercise

--- random/lc_random_test.go
+++ random/lc_random_test.go
@@ -10,3 +10,65 @@ package random
 func reverseLeftWords(s string, n int) string {
 	return s[n:] + s[:n]
 }
+
+/*
+* [LCP 01. 猜数字](https://leetcode-cn.com/problems/guess-numbers/)
+ */
+func game(guess []int, answer []int) int {
+	n := 0
+	for k, g := range guess {
+		if g == answer[k] {
+			n++
+		}
+	}
+	return n
+}
+
+/*
+* [771. 宝石与石头](https://leetcode-cn.com/problems/jewels-and-stones/)
+ */
+func numJewelsInStones(J string, S string) int {
+	/*
+		//时间O(J*S)，空间O(1)
+		n := 0
+		for _, s := range S {
+			for _, j := range J {
+				if s == j {
+					n++
+					break
+				}
+			}
+		}
+		return n
+	*/
+	// 时间O(J+S)，空间O(J)
+	n := 0
+	jm := make(map[rune]bool)
+	for _, j := range J {
+		jm[j] = true
+	}
+	for _, s := range S {
+		if _, ok := jm[s]; ok {
+			n++
+		}
+	}
+	return n
+}
+
+/*
+* [1281. 整数的各位积和之差](https://leetcode-cn.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/)
+ */
+func subtractProductAndSum(n int) int {
+	// 时间O(logn)，空间O(1)
+	// 对于时间复杂度：其中执行次数为 1+log10(n)，根据换底公式 1+log2(n) / log2(10)，即 Clogn + 1，因此复杂度为O(logn)
+	sum := 0
+	product := 1
+	temp := 0
+	for n != 0 {
+		temp = n % 10
+		sum += temp
+		product *= temp
+		n = n / 10
+	}
+	return product - sum
+}
