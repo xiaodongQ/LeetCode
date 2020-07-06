d4ac67c6f754bf7e7efdc7e2867f8143cb299d52
xiaodongQ <xiaodong0795@163.com>
2020-07-07 00:16:56 +0800

    [CHG]1. solved 14

--- lc/14.最长公共前缀_test.go
+++ lc/14.最长公共前缀_test.go
@@ -1,3 +1,10 @@
+package lc
+
+import (
+	"log"
+	"testing"
+)
+
 /*
  * @lc app=leetcode.cn id=14 lang=golang
  *
@@ -14,31 +21,118 @@
  * Testcase Example:  '["flower","flow","flight"]'
  *
  * 编写一个函数来查找字符串数组中的最长公共前缀。
- * 
+ *
  * 如果不存在公共前缀，返回空字符串 ""。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: ["flower","flow","flight"]
  * 输出: "fl"
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: ["dog","racecar","car"]
  * 输出: ""
  * 解释: 输入不存在公共前缀。
- * 
- * 
+ *
+ *
  * 说明:
- * 
+ *
  * 所有输入只包含小写字母 a-z 。
- * 
+ *
  */
+func TestLongestCommonPrefix(t *testing.T) {
+	log.Printf("res:%s", longestCommonPrefix([]string{
+		"aca", "cba",
+	}))
+}
 
 // @lc code=start
+// func longestCommonPrefix(strs []string) string {
+// 	if len(strs) == 0 {
+// 		return ""
+// 	}
+// 	// 找最短的字符串，从大到小依次找组合，组合存到一个map里
+// 	res := ""
+// 	minIndex := 0
+// 	minlen := len(strs[0])
+// 	for i, v := range strs {
+// 		if len(v) < minlen {
+// 			minIndex = i
+// 			minlen = len(v)
+// 		}
+// 	}
+// 	minstr := strs[minIndex]
+
+// 	log.Printf("minstr:%s, minlen:%d", minstr, minlen)
+// 	// 值为长度
+// 	m := map[string]int{}
+
+// 	for l := minlen; l > 0; l-- {
+// 		for i := 0; i < minlen-l+1; i++ {
+// 			prefix := minstr[i : i+l]
+// 			if _, ok := m[prefix]; ok {
+// 				continue
+// 			}
+// 			m[prefix] = 0
+
+// 			num := 0
+// 			for j := 0; j < len(strs); j++ {
+// 				if j == minIndex {
+// 					continue
+// 				}
+// 				if !strings.HasPrefix(strs[j], prefix) {
+// 					break
+// 				}
+// 				num++
+// 			}
+
+// 			if num == len(strs)-1 {
+// 				return prefix
+// 			}
+// 		}
+// 	}
+// 	return res
+// }
+
+/*
+	118/118 cases passed (0 ms)
+	Your runtime beats 100 % of golang submissions
+	Your memory usage beats 82.35 % of golang submissions (2.4 MB)
+	时间O(n*m)，n为字符串个数，m为最小串长度，空间O(1)
+*/
 func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	// 找最短的字符串，题意是只要判断前缀，而不需要判断中间包含相同串的情况
+	res := ""
+	minIndex := 0
+	minlen := len(strs[0])
+	for i, v := range strs {
+		if len(v) < minlen {
+			minIndex = i
+			minlen = len(v)
+		}
+	}
+	minstr := strs[minIndex]
 
+	for l := minlen; l > 0; l-- {
+		num := 0
+		for j := 0; j < len(strs); j++ {
+			if j == minIndex {
+				continue
+			}
+			if strs[j][:l] == minstr[:l] {
+				num++
+			}
+		}
+		if num == len(strs)-1 {
+			return minstr[:l]
+		}
+	}
+	return res
 }
-// @lc code=end
 
+// @lc code=end
--- lc/4.寻找两个正序数组的中位数_test.go
+++ lc/4.寻找两个正序数组的中位数_test.go
@@ -1,3 +1,5 @@
+package lc
+
 /*
  * @lc app=leetcode.cn id=4 lang=golang
  *
@@ -14,34 +16,34 @@
  * Testcase Example:  '[1,3]\n[2]'
  *
  * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
- * 
+ *
  * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
- * 
+ *
  * 你可以假设 nums1 和 nums2 不会同时为空。
- * 
- * 
- * 
+ *
+ *
+ *
  * 示例 1:
- * 
+ *
  * nums1 = [1, 3]
  * nums2 = [2]
- * 
+ *
  * 则中位数是 2.0
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * nums1 = [1, 2]
  * nums2 = [3, 4]
- * 
+ *
  * 则中位数是 (2 + 3)/2 = 2.5
- * 
- * 
+ *
+ *
  */
 
 // @lc code=start
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
+	return -1
 }
-// @lc code=end
 
+// @lc code=end
