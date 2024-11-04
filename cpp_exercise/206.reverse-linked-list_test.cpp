1ddb5a1660075c471c9ec71eda0f651f0809f6da
xiaodongQ <xiaodong0795@163.com>
2024-11-04 22:48:03 +0800

    反转链表

---
+++ cpp_exercise/206.reverse-linked-list_test.cpp
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=206 lang=cpp
+ *
+ * [206] Reverse Linked List
+ *
+ * https://leetcode.com/problems/reverse-linked-list/description/
+ *
+ * algorithms
+ * Easy (77.85%)
+ * Likes:    22005
+ * Dislikes: 468
+ * Total Accepted:    4.7M
+ * Total Submissions: 6M
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given the head of a singly linked list, reverse the list, and return the
+ * reversed list.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: head = [1,2]
+ * Output: [2,1]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: head = []
+ * Output: []
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of nodes in the list is the range [0, 5000].
+ * -5000 <= Node.val <= 5000
+ * 
+ * 
+ * 
+ * Follow up: A linked list can be reversed either iteratively or recursively.
+ * Could you implement both?
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        // 不需要虚拟头节点
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        ListNode* tmp = nullptr;
+        while (cur != nullptr) {
+            // 先记录下个节点，因为cur下面会改变，导致其next指向也变化
+            ListNode* tmp = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = tmp;
+        }
+        // 最后cur是nullptr，链表头应该为prev
+        return prev;
+    }
+};
+// @lc code=end
+
