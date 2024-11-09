65891d9f84d6f4884bf6bb2ed5e07f5eeb27cda2
xiaodongQ <xiaodong0795@163.com>
2024-11-09 21:48:29 +0800

    相交链表

--- cpp_exercise/160.相交链表_test.cpp
+++ cpp_exercise/160.相交链表_test.cpp
@@ -16,7 +16,46 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
+        int lenA = 0;
+        int lenB = 0;
+        ListNode* tmpA = headA;
+        ListNode* tmpB = headB;
+
+        // 计算链表长度
+        while (tmpA != nullptr) {
+            tmpA = tmpA->next;
+            lenA++;
+        }
+        while (tmpB != nullptr) {
+            tmpB = tmpB->next;
+            lenB++;
+        }
+        tmpA = headA;
+        tmpB = headB;
+
+        // 长链表先移动到两者长度相等的节点
+        int sub = lenA >= lenB ? (lenA-lenB) : (lenB-lenA);
+        if (lenA >= lenB) {
+            while (sub--) {
+                tmpA = tmpA->next;
+            }
+        } else {
+            while (sub--) {
+                tmpB = tmpB->next;
+            }
+        }
+
+        // 比较是否相交
+        while (tmpA != nullptr && tmpB != nullptr) {
+            if (tmpA == tmpB) {
+                // 相交
+                return tmpA;
+            }
+            tmpA = tmpA->next;
+            tmpB = tmpB->next;
+        }
+
+        return nullptr;
     }
 };
 // @lc code=end
