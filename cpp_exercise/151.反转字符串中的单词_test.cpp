418a17345d1a73276382fadeb3bafb32117911e8
xiaodongQ <xiaodong0795@163.com>
2024-11-27 23:10:43 +0800

    note

--- cpp_exercise/151.反转字符串中的单词_test.cpp
+++ cpp_exercise/151.反转字符串中的单词_test.cpp
@@ -7,67 +7,15 @@
 // @lc code=start
 class Solution {
 public:
-/*
-    // 方式1：先实现功能，借助语言内置的反转函数，并且不限制辅助空间占用。
-    string reverseWords(string s) {
-        // 1）字符串拆分成单词数组 2）数组反转 3）数组拼接
-        std::istringstream iss(s);
-        vector<string> words;
-        string word;
-        while (iss >> word) {
-            words.push_back(word);
-        }
-        // 反转，直接使用<algorithm>库中的std::reverse
-        std::reverse(words.begin(), words.end());
-        // 数组重新拼接
-        std::ostringstream oss;
-        for (auto i=0; i < words.size(); i++) {
-            if (i > 0) {
-                oss << " ";
-            }
-            oss << words[i];
-        }
-        return oss.str();
+    void removeExtraSpace(string &s) {
+
     }
-*/
-    // 方式2：借助双端队列实现反转
+
+    // 方式3：自行实现反转逻辑，辅助空间O(1)
     string reverseWords(string s) {
-        int left = 0;
-        int right = s.size() - 1;
-        // 去除前面空格，并记录有效起始位置left
-        while (left <= right && s[left] == ' ') {
-            left++;
-        }
-        // 去除后面空格，并记录有效终止位置right
-        while (left <= right && s[right] == ' ') {
-            right--;
-        }
-        // 中间处理，并检查空格
-        deque<string> words;
-        string word;
-        while (left <= right) {
-            char c = s[left];
-            // 新单词
-            if (!word.empty() && c == ' ') {
-                words.push_front(word);
-                word = "";
-            } else if (c != ' ') { // 此处单独区分' '，而不是仅else
-                word += c;
-            }
-            left++;
-        }
-        // 最后一个单词
-        words.push_front(word);
-        
-        // 借助string，拼接新字符串
-        string result;
-        for (auto i = 0; i < words.size(); i++) {
-            if (i > 0) {
-                result += " ";
-            }
-            result += words[i];
-        }
-        return result;
+        // 1）先去除多余空格
+        // 2）反转所有字符串
+        // 3）反转单词
     }
 };
 // @lc code=end
