027021b967f3030964b180c7aa9315c650bfaef5
xiaodongQ <xiaodong0795@163.com>
2024-11-28 22:38:13 +0800

    note

--- cpp_exercise/151.反转字符串中的单词_test.cpp
+++ cpp_exercise/151.反转字符串中的单词_test.cpp
@@ -7,9 +7,21 @@
 // @lc code=start
 class Solution {
 public:
+    // 移除多余空格
     void removeExtraSpace(string &s) {
-
+        int left = 0;
+        int right = s.size() - 1;
+        while (left <= right) {
+            if (left > 0 && s[left] == ' ' && s[left] == s[left-1]) {
+                s[left-1] = s[left];
+                continue;
+            }
+            left++;
+        }
     }
+
+    // 反转
+    void reverse()
 
     // 方式3：自行实现反转逻辑，辅助空间O(1)
     string reverseWords(string s) {
